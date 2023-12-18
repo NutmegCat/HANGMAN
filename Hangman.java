5046029f51ecb760fa8830eeec017726791dc350
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-12-17 21:50:33 -0500

    test if character input works 👍

--- Hangman.java
+++ Hangman.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("D:/OneDrive - TDSB/Desktop/ICS 2023-2024/HANGMAN/words.txt"));
+        Scanner keyboard = new Scanner(System.in);
 
         List<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
@@ -22,6 +23,12 @@ public class Hangman {
         
         List<Character> playerGuesses = new ArrayList<>();
         printWordState (word, playerGuesses);
+
+        System.out.println("Enter a character");
+        String letterGuesss = keyboard.nextLine();
+        playerGuesses.add(letterGuesss.charAt(0));
+
+        printWordState(word, playerGuesses);
     }
 
     private static void printWordState (String word, List<Character> playerGuesses) {
