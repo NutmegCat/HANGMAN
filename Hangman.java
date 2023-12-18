7f4be722e4d9e84f08f5a2a061e0dabeba32d3af
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-12-17 21:34:05 -0500

    check if words are spawning

--- Hangman.java
+++ Hangman.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
@@ -12,7 +13,10 @@ public class Hangman {
         while (scanner.hasNext()) {
             words.add(scanner.nextLine());
         }
+        Random rand = new Random();
 
-        
+        String word = words.get(rand.nextInt(words.size()));
+
+        System.out.println(word);
     }
 }
