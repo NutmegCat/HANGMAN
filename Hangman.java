5912837d4895d2357b2c0d622d350653faab9da3
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-12-17 21:31:21 -0500

    list called words

--- Hangman.java
+++ Hangman.java
@@ -1,13 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("D:/OneDrive - TDSB/Desktop/ICS 2023-2024/HANGMAN/words.txt"));
 
+        List<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
         }
+
+        
     }
 }
