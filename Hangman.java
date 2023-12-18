ecd62e5a333f5fbfbd86604098133e44de6ec6e3
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-12-17 21:28:35 -0500

    file import

--- Hangman.java
+++ Hangman.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Hangman {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("D:/OneDrive - TDSB/Desktop/ICS 2023-2024/HANGMAN/words.txt"));
     }
 }
