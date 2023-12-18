0e86bd24bfb93f7947e79a5cb91427473eb1f135
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-12-17 21:29:58 -0500

    test dictionary

--- Hangman.java
+++ Hangman.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("D:/OneDrive - TDSB/Desktop/ICS 2023-2024/HANGMAN/words.txt"));
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
